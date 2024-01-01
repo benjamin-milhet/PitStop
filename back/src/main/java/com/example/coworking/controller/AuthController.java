d4dbfd7dede5060441e51244f68906f6f8fcda01
milhet <benjamin.milhet@outlook.fr>
2024-01-01 19:29:53 +0100

    fix bug logout

--- back/src/main/java/com/example/coworking/controller/AuthController.java
+++ back/src/main/java/com/example/coworking/controller/AuthController.java
@@ -6,14 +6,18 @@ import com.example.coworking.payload.request.RegisterRequest;
 import com.example.coworking.payload.response.AuthenticationResponse;
 import com.example.coworking.payload.response.RefreshTokenResponse;
 import com.example.coworking.service.AuthenticationService;
+import com.example.coworking.service.JwtService;
 import com.example.coworking.service.RefreshTokenService;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -23,6 +27,8 @@ public class AuthController {
 
     private final AuthenticationService authenticationService;
     private final RefreshTokenService refreshTokenService;
+    private final AuthenticationManager authenticationManager;
+    private final JwtService jwtService;
 
     @PostMapping("/login")
     public ResponseEntity<AuthenticationResponse> login(@RequestBody AuthenticationRequest request) {
@@ -37,5 +43,36 @@ public class AuthController {
     @PostMapping("/refresh-token")
     public ResponseEntity<RefreshTokenResponse> refreshToken(@RequestBody RefreshTokenRequest request) {
         return ResponseEntity.ok(refreshTokenService.generateNewToken(request));
+    }
+
+    @PostMapping("/refresh-token-cookie")
+    public ResponseEntity<Void> refreshTokenCookie(HttpServletRequest request) {
+        String refreshToken = refreshTokenService.getRefreshTokenFromCookies(request);
+        RefreshTokenResponse refreshTokenResponse = refreshTokenService
+                .generateNewToken(new RefreshTokenRequest(refreshToken));
+        ResponseCookie NewJwtCookie = jwtService.generateJwtCookie(refreshTokenResponse.getAccessToken());
+        return ResponseEntity.ok()
+                .header(HttpHeaders.SET_COOKIE, NewJwtCookie.toString())
+                .build();
+    }
+    @GetMapping("/info")
+    public Authentication getAuthentication(@RequestBody AuthenticationRequest request){
+        return     authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(request.getEmail(),request.getPassword()));
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<Void> logout(HttpServletRequest request){
+        String refreshToken = refreshTokenService.getRefreshTokenFromCookies(request);
+        if(refreshToken != null) {
+            refreshTokenService.deleteByToken(refreshToken);
+        }
+        ResponseCookie jwtCookie = jwtService.getCleanJwtCookie();
+        ResponseCookie refreshTokenCookie = refreshTokenService.getCleanRefreshTokenCookie();
+        return ResponseEntity.ok()
+                .header(HttpHeaders.SET_COOKIE,jwtCookie.toString())
+                .header(HttpHeaders.SET_COOKIE,refreshTokenCookie.toString())
+                .build();
+
     }
 }
